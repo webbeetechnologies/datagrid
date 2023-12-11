9aa7e95010c1bb7a151e68265350a3db33d6e5be
Thet Aung <thetaung.dev@gmail.com>
2023-12-11 09:02:26 +0300

    fix: type issues, eslint issue

--- src/components/Grid/FillHandle.tsx
+++ src/components/Grid/FillHandle.tsx
@@ -1,37 +1,36 @@
-import React from "react";
-import { ShapeConfig } from "konva/lib/Shape";
+import type { ShapeConfig } from 'konva/lib/Shape';
 
 /**
  * Fill handle component
  */
-const FillHandle: React.FC<ShapeConfig> = ({
-  x = 0,
-  y = 0,
-  stroke,
-  strokeWidth = 1,
-  size = 8,
-  borderColor,
-  ...props
-}) => {
-  if (x === 0 || y === 0) return null;
-  return (
-    <div
-      style={{
-        position: "absolute",
-        left: x - size / 2,
-        top: y - size / 2,
-        width: size,
-        height: size,
-        border: `${strokeWidth}px ${borderColor} solid`,
-        borderRightWidth: 0,
-        borderBottomWidth: 0,
-        background: stroke as string,
-        cursor: "crosshair",
-        pointerEvents: "all",
-      }}
-      {...props}
-    />
-  );
+const FillHandle = ({
+    x = 0,
+    y = 0,
+    stroke,
+    strokeWidth = 1,
+    size = 8,
+    borderColor,
+    ...props
+}: ShapeConfig) => {
+    if (x === 0 || y === 0) return null;
+    return (
+        <div
+            style={{
+                position: 'absolute',
+                left: x - size / 2,
+                top: y - size / 2,
+                width: size,
+                height: size,
+                border: `${strokeWidth}px ${borderColor} solid`,
+                borderRightWidth: 0,
+                borderBottomWidth: 0,
+                background: stroke as string,
+                cursor: 'crosshair',
+                pointerEvents: 'all',
+            }}
+            {...props}
+        />
+    );
 };
 
 export default FillHandle;
