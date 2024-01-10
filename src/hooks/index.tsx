355dec2826681bb5a887c679a1b9a851a061c2ac
Thet Aung <thetaung.dev@gmail.com>
2024-01-10 14:34:27 +0300

    fix: updated index files, removed unnecessary exports

--- src/hooks/index.tsx
+++ src/hooks/index.tsx
@@ -1,11 +1,11 @@
-export { default as useContextMenu, Props as UseContextMenuProps } from './useContextMenu';
-export {
-    default as useHandleKeydownEvents,
-    Props as UseHandleKeydownEventsProps,
-} from './useHandleKeydownEvents';
-export { default as useHandleClickOutside } from './useHandleClickOutside';
-export { default as useDatagridMethods } from './useDatagridMethods';
-export { default as useToggleCellEditingState } from './useToggleCellEditingState';
+// export { default as useContextMenu, Props as UseContextMenuProps } from './useContextMenu';
+// export {
+//     default as useHandleKeydownEvents,
+//     Props as UseHandleKeydownEventsProps,
+// } from './useHandleKeydownEvents';
+// export { default as useHandleClickOutside } from './useHandleClickOutside';
+// export { default as useDatagridMethods } from './useDatagridMethods';
+// export { default as useToggleCellEditingState } from './useToggleCellEditingState';
 export { default as useSelection, UseSelectionOptions, SelectionResults } from './useSelection';
 export {
     default as useEditable,
