15d0c13290e4a9ca11f4c4733053b97ff0b2174a
Thet Aung <thetaung.dev@gmail.com>
2024-12-21 13:58:13 +0300

    improvements: updated lru-cache

--- src/utils/getTextWidth.ts
+++ src/utils/getTextWidth.ts
@@ -1,9 +1,11 @@
 import { autoSizerCanvas } from './autoSizerCanvas';
-import LRU from 'lru-cache';
+import { LRUCache } from 'lru-cache';
 import type { IWrapTextResultProps } from './types';
 
-const fontCache: { [key: string]: LRU<string, number> } = {};
-export const textDataCache = new LRU<string, IWrapTextResultProps>(500);
+const fontCache: { [key: string]: LRUCache<string, number> } = {};
+export const textDataCache = new LRUCache<string, IWrapTextResultProps>({
+    max: 500,
+});
 
 export const getTextWidth = (text: string, font: string) => {
     let width: number | undefined = 0;
@@ -12,7 +14,7 @@ export const getTextWidth = (text: string, font: string) => {
     }
     let cacheOfFont = fontCache[font];
     if (!cacheOfFont) {
-        cacheOfFont = fontCache[font] = new LRU(500);
+        cacheOfFont = fontCache[font] = new LRUCache({ max: 500 });
     }
     width = cacheOfFont.get(text);
     if (width == null) {
